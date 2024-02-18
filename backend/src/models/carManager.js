34c431e8b255277d6f46a928333cb67b72d8e14d
bravax <xavierlobez@hotmail.fr>
2024-02-18 18:19:42 +0100

    Cleaning

--- backend/src/models/carManager.js
+++ backend/src/models/carManager.js
@@ -2,48 +2,23 @@ const AbstractManager = require("./AbstractManager");
 
 class CarManager extends AbstractManager {
   constructor() {
-    // Call the constructor of the parent class (AbstractManager)
-    // and pass the table name "item" as configuration
     super({ table: "cars" });
   }
 
-  // The C of CRUD - Create operation
-
-  // Return the ID of the newly inserted item
-  // The Rs of CRUD - Read operations
-
   async read(id) {
-    // Execute the SQL SELECT query to retrieve a specific item by its ID
     const [rows] = await this.database.query(
       `select * from ${this.table} where id = ?`,
       [id]
     );
 
-    // Return the first row of the result, which represents the item
     return rows[0];
   }
 
   async readAll() {
-    // Execute the SQL SELECT query to retrieve all items from the "item" table
     const [rows] = await this.database.query(`select * from ${this.table}`);
 
-    // Return the array of items
     return rows;
   }
-
-  // The U of CRUD - Update operation
-  // TODO: Implement the update operation to modify an existing item
-
-  // async update(item) {
-  //   ...
-  // }
-
-  // The D of CRUD - Delete operation
-  // TODO: Implement the delete operation to remove an item by its ID
-
-  // async delete(id) {
-  //   ...
-  // }
 }
 
 module.exports = CarManager;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,27 @@ import "./App.scss";
 function App() {
   return (
     <main>
-      <section className="title">
+      <header className="title">
         <h1>Véhicules d'exception et de légende</h1>
-      </section>
+      </header>
       <section className="motor-180">
+        <p className="object">
+          Ce site est dédié à la fin de ma formation théorique de développeur
+          web, web mobile. Il symbolise le commencement d'une nouvelle aventure,
+          présentant une sélection minutieuse de voitures anciennes, reflétant{" "}
+          <strong>mes choix</strong> personnels, ainsi que{" "}
+          <strong>l'histoire </strong>captivante d'une restauration de voiture
+          inspirée du travail d'un restaurateur de voitures anciennes.
+          <br />
+          Cette expérience fascinante débute simultanément avec le début de ma
+          nouvelle carrière.
+          <br /> Il est à noter que ce site continuera de progresser et de se
+          développer au fil du temps.
+        </p>
         <img src={Cadillac} alt="vue de face d'une cadillac" />
         <Link to="/ranking">
           <img src={roofTop} alt="Coffre de toit" className="roof-top" />
-          <strong className="text-rf">Un classement</strong>
+          <strong className="text-rf">Mes choix</strong>
         </Link>
 
         <Link to="/history">
